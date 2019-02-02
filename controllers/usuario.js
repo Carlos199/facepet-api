d71c821b6f88f0ec109506fe7493c9d2303062b4
Jackson Huang <jacksonh201325@gmail.com>
2019-02-02 12:32:40 -0300

    se agrego la funcionalidad Log in

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -59,4 +59,18 @@ module.exports = {
             res.status(500).json(err);
         });
     },
+
+    login(req, res) {
+        Usuario.find({
+            correo: req.body.correo,
+            clave: req.body.clave
+        })
+            .then(function (result) {
+                res.status(200).json(result);
+            }) 
+            .catch(function (err) {
+                res.status(500).json(err);
+            });
+    },
+
 };
